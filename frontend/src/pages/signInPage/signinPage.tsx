088a803853aba4ced9c7525b9b71a82c6a89b231
nbhd12 <nupur124@gmail.com>
2025-10-14 14:34:17 +0200

    signInWIP

--- frontend/src/pages/signInPage/signinPage.tsx
+++ frontend/src/pages/signInPage/signinPage.tsx
@@ -1,1 +1,40 @@
 //sampletext
+import { useState } from "react";
+import "./signinPage.css";
+import type {ApiUserResult} from "../../../";
+import {useAuthContext} from "../../../core/AuthContext";
+import {useNavigate} from "react-router";
+
+
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
+const defSignInFormData: SignInFormData ={
+    email: "",
+    password: "",
+};
+
+export const SignIn = () => {
+    const [formData,setFormData] = useState<SignInFormData> (defSignInFormData);
+    const [submitError, setSubmitError] = useState <string | null> (null);
+    
+    const {setUser, setToken} = useAuthContext();
+    const navigate = useNavigate();
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name,value} = event.target;
+
+    setFormData((prev) => ({...prev, [name]: value }));
+    };
+
+    const handleSubmit = async (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try{
+            const resposne = await fetch ()
+        }
+    }
+
+} 
